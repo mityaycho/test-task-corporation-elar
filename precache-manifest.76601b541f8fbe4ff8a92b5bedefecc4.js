d83f155a751818c6c913f69d85a0bc086f3bebcc
mityaycho <mityaycho@gmail.com>
2020-02-21 00:43:13 +0300

    Updates

---
+++ precache-manifest.76601b541f8fbe4ff8a92b5bedefecc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dae6557ee2b0c2eda3d68e98c4ec3407",
+    "url": "/test-task-corporation-elar/index.html"
+  },
+  {
+    "revision": "c26f109f893700d4461d",
+    "url": "/test-task-corporation-elar/static/css/main.19a79720.chunk.css"
+  },
+  {
+    "revision": "bffb5abe631056665d93",
+    "url": "/test-task-corporation-elar/static/js/2.e47610f1.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/test-task-corporation-elar/static/js/2.e47610f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c26f109f893700d4461d",
+    "url": "/test-task-corporation-elar/static/js/main.d0aa7d29.chunk.js"
+  },
+  {
+    "revision": "7d306bee85f7454199b5",
+    "url": "/test-task-corporation-elar/static/js/runtime-main.95204044.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-task-corporation-elar/static/media/logo.5d5d9eef.svg"
+  }
+]);
